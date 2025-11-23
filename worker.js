c42636deefd4e424085f1aa8361e80f3f808164b
1355600640 <1355600640@qq.com>
2025-11-23 22:34:33 +0800

    refactor(worker): 优化静态资源请求逻辑，简化错误处理流程
    - 移除 serveStatic 依赖，改为直接用 env.ASSETS.fetch 请求资源
    - 判断资源请求是否返回 404，非 404 则直接返回资源响应
    - 对非静态资源统一返回 index.html 实现 SPA 路由回退
    - 清理 wrangler.jsonc，增加 main 字段指向入口文件 worker.js
    - 去除多余配置项，精简配置文件结构

--- worker.js
+++ worker.js
@@ -1,12 +1,13 @@
-import { serveStatic } from '@cloudflare/kv-asset-handler'
-
 export default {
   async fetch(request, env) {
-    try {
-      return await serveStatic(request, env.ASSETS)
-    }
-    catch {
-      return await env.ASSETS.fetch(new Request('/index.html', request))
-    }
+    // const url = new URL(request.url);
+
+    // 如果请求静态文件则直接返回
+    const res = await env.ASSETS.fetch(request)
+    if (res.status !== 404)
+      return res
+
+    // 如果不是静态资源 → 始终返回 index.html (SPA fallback)
+    return await env.ASSETS.fetch(new Request('/index.html', request))
   }
 }
